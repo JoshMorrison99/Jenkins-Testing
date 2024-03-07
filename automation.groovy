6b22093fbf564962a521495edbdb3414ebecbaf1
JoshMorrison99 <josh.morrison1999@gmail.com>
2024-03-07 08:00:18 -0500

    Update automation.groovy

--- automation.groovy
+++ automation.groovy
@@ -1,1 +1,11 @@
-echo currentBuild.displayName
+node {
+  stage("Create File") {
+    echo currentBuild.displayName > my_file.txt
+  }
+
+  stage("Artifact File") {
+    archiveArtifacts artifacts: 'my_file.txt', onlyIfSuccessful: true
+  }
+}
+
+
